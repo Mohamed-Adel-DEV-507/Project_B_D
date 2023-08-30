a5410062e6b5ff4f295c07bb9289e33d4ac7e3af
Mohamed-Adel-DEV-507 <115120909+Mohamed-Adel-DEV-507@users.noreply.github.com>
2023-08-30 19:12:55 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+var x1 = "First" + " " + "number" + " " + "is:";
+var x11 =10;
+var x2 = "Second" + " " + "number" + " " + "is:";
+var x22 =4;
+
+console.log(x1 + x11);
+console.log(x2 + x22);
+
+var x3 = x11 + x22;
+console.log("sumn" + " " + "is:" + " " + x3);
+
+var x4 = x11 - x22;
+console.log("sub" + " " + "is:" + " " + x4);
+
+
+var x5 = x11 * x22;
+console.log("Mult" + " " + "is:" + " " + x5);
+
+var x6 = x11 / x22;
+    console.log("Div" + " " + "is:" + " " + x6);
+
+
+var x7 = x11 % x22;
+    console.log("Mod" + " " + "is:" + " " + x7);
+
+
+    document.write(x1,(" "),x11,(" "),x2,(" "),x22,("sumn" + " " + "is:" + " " + x3),(" "),("sub" + " " + "is:" + " " + x4),(" "),("Mult" + " " + "is:" + " " + x5),(" "),("Div" + " " + "is:" + " " + x6),(" "),("Mod" + " " + "is:" + " " + x7));
+
